808f6fe6495882590ee50600968998b28cd04c80
Tyler Stevens <tyler.stevens.dev@protonmail.com>
2022-04-13 06:39:43 -0700

    Removed 2u backspace from LAYOUT_all

--- keyboards/keystonecaps/gameroyadvance/gameroyadvance.h
+++ keyboards/keystonecaps/gameroyadvance/gameroyadvance.h
@@ -41,7 +41,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 }
 
 #define LAYOUT_all(                                                                                        \
-	L00, L01, L02, L03, L04, L05, L06,                        R00, R01, R02, R03, R04, R05, R06, R07, R08, \
+	L00, L01, L02, L03, L04, L05, L06,                        R00, R01, R02, R03, R04, R05, R06,      R08, \
 	L10, L11, L12, L13, L14, L15, L16, L17, L18,    R10, R11, R12, R13, R14, R15, R16, R17, R18,      R28, \
 	L20, L21, L22, L23, L24, L25,      L27, L28,    R20, R21, R22, R23, R24, R25, R26, R27,      R38,      \
 	L30, L44, L31, L32, L33, L34, L35, L37, L38,    R30, R31, R32, R33, R34, R35, R36, R37,      R46, R48, \
@@ -54,7 +54,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	{ L20,   L21,   L22,   L23,   L24,   L25,   KC_NO, L27,   L28   }, \
 	{ L30,   L31,   L32,   L33,   L34,   L35,   KC_NO, L37,   L38   }, \
 	{ L40,   L41,   L42,   L43,   L44,   L45,   KC_NO, L47,   L48   }, \
-	{ R00,   R01,   R02,   R03,   R04,   R05,   R06,   R07,   R08   }, \
+	{ R00,   R01,   R02,   R03,   R04,   R05,   R06,   KC_NO, R08   }, \
 	{ R10,   R11,   R12,   R13,   R14,   R15,   R16,   R17,   R18   }, \
 	{ R20,   R21,   R22,   R23,   R24,   R25,   R26,   R27,   R28   }, \
 	{ R30,   R31,   R32,   R33,   R34,   R35,   R36,   R37,   R38   }, \
